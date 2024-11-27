a094353c5a292a796d97097969ed8b6f008b6da6
Abhishek Sangam <abhishekasangam@gmail.com>
2024-11-28 00:52:24 +0530

    Regrouping files

--- TradingSystem/Assets/Scripts/GameplayManager.cs
+++ TradingSystem/Assets/Scripts/GameplayManager.cs
@@ -3,7 +3,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Manager : MonoBehaviour
+public class GameplayManager : MonoBehaviour
 {
     [SerializeField] private PlayerController playerController;
     public ShopInventoryView shopInventoryView;
